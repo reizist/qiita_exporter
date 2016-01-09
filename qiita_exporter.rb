f1a01fd93121c21dc476ec3dfeccd0f088605a64
reizist <reizist@gmail.com>
2016-01-10 04:27:12 +0900

    Initial Commit

---
+++ qiita_exporter.rb
@@ -0,0 +1,81 @@
+require 'qiita'
+require 'pry'
+require 'fileutils'
+require 'yaml'
+
+ACCESS_TOKEN = ENV['QIITA_ACCESS_TOKEN']
+TEAM_DOMAIN = ENV['QIITA_TEAM_DOMAIN']
+ARTICLE_DIR = 'posts/'
+META_DIR = 'meta/'
+EXT = '.md'
+META_EXT = '.yml'
+
+class QiitaExporter
+  def initialize(access_token, team_domain)
+    @client = Qiita::Client.new(access_token: access_token, team: team_domain)
+    @dir = ARTICLE_DIR
+    @ext = EXT
+    @meta_ext = META_EXT
+    @meta_dir = META_DIR 
+  end
+
+  def export(page_num: 1, per_page: 10)
+    FileUtils.mkdir_p(@dir)
+    FileUtils.mkdir_p(@meta_dir)
+    (1..page_num).each do |i|
+      @client.list_items(page: i, per_page: per_page).body.each do |article|
+        to_file(article)
+      end
+    end
+  end
+
+  private
+
+  def to_file(article)
+    meta = unravel(article)
+    save_markdown(meta)
+    save_meta(meta)
+  end
+
+  def save_meta(meta)
+    formatted_meta = {
+      meta[:id] => {
+        'title' => meta[:title],
+        'tags' => meta[:tags].map{|i| i["name"]},
+        'user' => meta[:user]
+      }
+    }
+    
+    file_name = "#{@meta_dir}#{meta[:id]}#{@meta_ext}"
+    FileUtils.touch(file_name) unless File.exist?(file_name)
+    File.open(file_name, 'w') do |f|
+      YAML.dump(formatted_meta, f)
+    end
+  end
+
+  def save_markdown(meta)
+    file_name = "#{@dir}#{meta[:id]}#{@ext}"
+    FileUtils.touch(file_name) unless File.exist?(file_name)
+    File.open(file_name, 'w').write(meta[:body])
+  end
+
+  def unravel(article)
+    id = article['id']
+    title = article['title']
+    body = article['body']
+    tags = article['tags']
+    user = article['user']['id']
+    user_image = article['user']['profile_image_url']
+    {
+     id: id,
+     title: title,
+     body: body,
+     user: user, 
+     user_image: user_image,
+     tags: tags
+    }
+  end
+
+end
+
+QiitaExporter.new(ACCESS_TOKEN, TEAM_DOMAIN).export
